bed2f2bd8d72429c3876d07b223fd7b1e72bb8cc
Subhranil <subhranilroy5@gmail.com>
2020-04-09 09:17:06 +0530

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  host: {
+		"[class.work-management]": "( theme === 'WMgt' )",
+		"[class.qpp]": "( theme === 'Qpp' )"
+	},
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'themepoc';
+  theme:string;
+
+  @ViewChild('addtheme') containerdiv:ElementRef;
+
+  addWMtheme(){
+    
+    this.theme="WMgt";
+  }
+  addQPPtheme(){
+    
+    this.theme="Qpp";
+  }
 }
--- src/app/theme-one-test/themeone/themeone.component.ts
+++ src/app/theme-one-test/themeone/themeone.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import {} from '../theme'
+
 @Component({
   selector: 'app-themeone',
   templateUrl: './themeone.component.html',
